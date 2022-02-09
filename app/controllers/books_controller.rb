fbea2bdd6bb1e194c4a8e9fed2a229dcc12235a1
EC2 Default User <ec2-user@ip-172-31-6-219.ec2.internal>
2022-02-09 16:10:04 +0000

    エラーメッセージ・サクセスメッセージの表示を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,19 @@ class BooksController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    # # 3. データをデータベースに保存するためのsaveメソッド実行
+    # book.save
+    # # 4. トップ画面へリダイレクト
+    # redirect_to book_path(book.id)
+
+    if @book.save
+      redirect_to book_path(@book.id) , notice: "Book was successfully created."
+    else
+      @books=Book.all
+      render :index
+    end
+
   end
 
   def show
@@ -26,15 +34,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id),notice: "Book was successfully updated."
+    else
+      render :edit
+    end
+
   end
 
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+    redirect_to '/books' ,notice: "Book was successfully destroyed." # 投稿一覧画面へリダイレクト
   end
 
   private
